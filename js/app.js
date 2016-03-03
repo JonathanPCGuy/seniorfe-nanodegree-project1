eb2b257d649a3ddc088bcb8fb1cc018461cebe5c
Jonathan Lam <jlampcguy@gmail.com>
2016-03-02 22:42:54 -0600

    Basic checkpoint and rust shaking

--- js/app.js
+++ js/app.js
@@ -2,71 +2,88 @@ var myapp = myapp || {};
 
 // is this how to do namespace?
 
-myapp.FormViewModel = function () {
-    var self = this;
+// ok how to do this with knockout or backbone or whatever
 
-    self.formPages = $('section.single-page');
+// this whole project is ugly. need to shake off a lot of rust
 
-    self.lastPage = 2;
-    self.currentPage = ko.observable(0);
+var submitButton = $('#submit');
+// not sure how to hold these values; tbd
+var locationField = $('#location-field')[0];
 
-    self.changeFormPage = function (data, event) {
-        var optionClicked = $(event.target).attr('class');
-        if (optionClicked === 'previous' && self.currentPage() > 0) {
-            self.currentPage(self.currentPage() - 1);
-        }
-        else if (optionClicked === 'next' && self.currentPage() < self.lastPage) {
-            self.currentPage(self.currentPage() + 1);
-        }
-    };
-
-    self.updateView = ko.computed(function () {
-        // assumption: jQuery panels in order
-        // may not always be the case! will need to use other stuff to fix things down
-        // but for this homework it is ok
-        var visiblePage = self.currentPage();
-        $(self.formPages[visiblePage]).removeClass('hidden');
-        self.formPages.each(function (index, value) {
-            // each doesn't need closure?
-            if (visiblePage !== index) {
-                $(value).addClass('hidden');
-            }
-        });
-    });
-
-    self.stepText = ko.computed(function () {
-        // probably not good to embed the actual raw text here?
-        if (self.currentPage() === 0) {
-            return 'Shipping Info';
-        }
-        else if (self.currentPage() === 1) {
-            return 'Billing Info';
-        }
-        else {
-            return 'Order Verification';
-        }
-    });
-
-    self.getRawBarWidth = ko.computed(function() {
-        return Math.round((self.currentPage() / (self.lastPage + 1) * 100));
-    });
-
-    self.getBarWidth = ko.computed(function () {
-        var value = self.getRawBarWidth();
-            return value.toString();
-    });
-
-    self.getBarWidthWithPercentage = ko.computed(function() {
-        return self.getRawBarWidth() + "%";
-    });
-    // is it possible to specify a data-bind in html
-    // but not declare it as a variable in js?
-};
-
-$(function () {
-    // lazyness
-    var formLabels = $('label');
-    formLabels.toggleClass('col-md-9');
-    formLabels.toggleClass('col-md-offset-3');
-    ko.applyBindings(new myapp.FormViewModel());
+submitButton.click(function() {
+    // reset fields
+    locationField.setCustomValidity('');
+    var testtest = locationField.value;
+    if(locationField.value.length == 0) {
+        locationField.setCustomValidity('Please specify a location');
+    }
 });
+
+// myapp.FormViewModel = function () {
+//     var self = this;
+// 
+//     self.formPages = $('section.single-page');
+// 
+//     self.lastPage = 2;
+//     self.currentPage = ko.observable(0);
+// 
+//     self.changeFormPage = function (data, event) {
+//         var optionClicked = $(event.target).attr('class');
+//         if (optionClicked === 'previous' && self.currentPage() > 0) {
+//             self.currentPage(self.currentPage() - 1);
+//         }
+//         else if (optionClicked === 'next' && self.currentPage() < self.lastPage) {
+//             self.currentPage(self.currentPage() + 1);
+//         }
+//     };
+// 
+//     self.updateView = ko.computed(function () {
+//         // assumption: jQuery panels in order
+//         // may not always be the case! will need to use other stuff to fix things down
+//         // but for this homework it is ok
+//         var visiblePage = self.currentPage();
+//         $(self.formPages[visiblePage]).removeClass('hidden');
+//         self.formPages.each(function (index, value) {
+//             // each doesn't need closure?
+//             if (visiblePage !== index) {
+//                 $(value).addClass('hidden');
+//             }
+//         });
+//     });
+// 
+//     self.stepText = ko.computed(function () {
+//         // probably not good to embed the actual raw text here?
+//         if (self.currentPage() === 0) {
+//             return 'Shipping Info';
+//         }
+//         else if (self.currentPage() === 1) {
+//             return 'Billing Info';
+//         }
+//         else {
+//             return 'Order Verification';
+//         }
+//     });
+// 
+//     self.getRawBarWidth = ko.computed(function() {
+//         return Math.round((self.currentPage() / (self.lastPage + 1) * 100));
+//     });
+// 
+//     self.getBarWidth = ko.computed(function () {
+//         var value = self.getRawBarWidth();
+//             return value.toString();
+//     });
+// 
+//     self.getBarWidthWithPercentage = ko.computed(function() {
+//         return self.getRawBarWidth() + "%";
+//     });
+//     // is it possible to specify a data-bind in html
+//     // but not declare it as a variable in js?
+// };
+// 
+// $(function () {
+//     // lazyness
+//     var formLabels = $('label');
+//     formLabels.toggleClass('col-md-9');
+//     formLabels.toggleClass('col-md-offset-3');
+//     ko.applyBindings(new myapp.FormViewModel());
+// });
